fd62d8a2e4179a2b7b1851a1b7cb089e16f526d0
domi555 <66876360+domi555@users.noreply.github.com>
2021-10-06 11:41:33 +0200

    Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const path = require('path');
+const cors = require('cors');
 
 require('colors');
 require('dotenv').config();
@@ -15,6 +16,8 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(helmet());
+
+app.use(cors());
 
 app.use(express.json());
 
--- model/cars.js
+++ model/cars.js
@@ -1,7 +1,9 @@
 const db = require('../db');
 
 const getCars = async () => {
-  const { rows } = await db.query('SELECT * FROM cars');
+  const { rows } = await db.query(
+    'SELECT * FROM cars JOIN owner ON cars.owner = owner.id',
+  );
   return {
     code: 200,
     data: rows,
